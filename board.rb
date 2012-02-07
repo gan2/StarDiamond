b8506e4b37ca746192639706814926f17c63fdcd
Gantz Ruby <gan2.ruby@gmail.com>
2012-02-07 16:34:25 +0900

    Board クラスを作成

---
+++ board.rb
@@ -0,0 +1,39 @@
+#!c:/Ruby/bin/ruby -Ku
+$KCODE = 'u'
+
+require 'pp'
+require 'kconv'
+
+class Board
+  # size = トライアングルの辺の長さ
+  def initialize(size)
+    @nodes = []
+    # ボードの幅
+    @width = (size * 2 - 1) * 3 - 2
+    # ボードの高さはトライアングルの辺の長さ * 3 から重複分の 3 を引いたもの
+    @height = size * 4 - 3
+
+    @height.times do
+      ary = []
+      @width.times { ary << nil }
+      @nodes << ary
+    end
+  end
+
+  def display
+    @nodes
+  end
+  
+  class Node
+    def initialize(start, goal, toreru_color, piece)
+      @start = start
+      @goal  = goal
+      @toreru_color = toreru_color
+      @piece = piece
+    end
+
+    def inspect
+      
+    end
+  end
+end
